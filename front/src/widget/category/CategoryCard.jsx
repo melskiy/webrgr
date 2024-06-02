fa94b3e88eaa83d6608545160de753201a1aa99f
todarenko.03 <todarenkov@mail.ru>
2024-06-02 10:04:01 +0600

    completed front with mocks

--- front/src/widget/category/CategoryCard.jsx
+++ front/src/widget/category/CategoryCard.jsx
@@ -1,38 +1,80 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./CategoryCard.css"
 
 const CategoryCard = () => {
-    // useEffect(() => (
-    //     fetch("")
-    // ))
+    useEffect(() => {
+        fetch("/src/entities/products/products.json")
+            .then(response => response.json())
+            .then(response => setProducts(response))
+    }, [])
+    const [products, setProducts] = useState();
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascenging' });
+
+    const sortData = (key) => {
+        let direction = 'ascenging';
+        if (sortConfig.key === key & sortConfig.direction === 'ascenging') {
+            direction = 'descenging'
+        }
+
+        const sorted = [...products].sort((a, b) => {
+            if (a[key] < b[key]) {
+                return direction === 'ascenging' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+                return direction === 'ascenging' ? 1 : -1;
+            }
+            return 0;
+        });
+
+        setProducts(sorted);
+        setSortConfig({ key, direction });
+    }
+
     return (
         <div className="category-card">
             <h1 className="category-card__title">НАЗВАНИЕ КАТЕГОРИИ</h1>
             <table>
                 <thead>
                     <tr>
-                        <th>Продукт</th>
-                        <th>Калории</th>
-                        <th>Белки</th>
-                        <th>Жиры</th>
-                        <th>Углеводы</th>
+                        <th>
+                            Продукт
+                        </th>
+                        <th>
+                            <div className="category-card__th-block">
+                                <p>Калории</p>
+                                <button onClick={() => sortData('calories')}></button>
+                            </div>
+                        </th>
+                        <th>
+                            <div className="category-card__th-block">
+                                <p>Белки</p>
+                                <button onClick={() => sortData('squirrels')}></button>
+                            </div>
+                        </th>
+                        <th>
+                            <div className="category-card__th-block">
+                                <p>Жиры</p>
+                                <button onClick={() => sortData('fats')}></button>
+                            </div>
+                        </th>
+                        <th>
+                            <div className="category-card__th-block">
+                                <p>Углеводы</p>
+                                <button onClick={() => sortData('carbohydrates')}></button>
+                            </div>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Пример продукта 1</td>
-                        <td>100</td>
-                        <td>5</td>
-                        <td>3</td>
-                        <td>10</td>
-                    </tr>
-                    <tr>
-                        <td>Пример продукта 2</td>
-                        <td>150</td>
-                        <td>7</td>
-                        <td>5</td>
-                        <td>20</td>
-                    </tr>
+                    {products?.map((product) => (
+                        <tr key={product.id}>
+                            <td>{product.product}</td>
+                            <td>{product.calories}</td>
+                            <td>{product.squirrels}</td>
+                            <td>{product.fats}</td>
+                            <td>{product.carbohydrates}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>

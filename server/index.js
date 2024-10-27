d94f2bc6de894246c291296c790d544e3b3d0d0b
malickijaleksandr <cfif5665@gmail.com>
2024-10-28 02:37:25 +0600

    1st lab

--- server/index.js
+++ server/index.js
@@ -1,39 +1,68 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const app = express();
 const port = 4000;
 
-app.listen(port, (error) =>{ 
-    if(!error) 
-        console.log("Server is Successfully Running, and App is listening on port "+ port) 
-    else 
-        console.log("Error occurred, server can't start", error); 
-    } 
-); 
-
-const uri = "mongodb+srv://cfif5665:OMJ9CaMHV6wa2fKR@cluster0.d8cqo4s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect();
-db = client.db('products');
+// Подключение к MongoDB
+const uri = "mongodb+srv://cfif5665:OMJ9CaMHV6wa2fKR@cluster0.d8cqo4s.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.use(express.json());
 
-app.all('/collection/:collectionName', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+// Определение схемы пользователя
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.use(cors());
+
+const db = mongoose.connection;
 
 app.get('/collection/:collectionName', async (req, res) => {
-    try {
-      const collectionName = req.params.collectionName;
-      const products = await db.collection(collectionName).find().toArray();
-      if (products.length > 0) {
-        res.json(products);
-      } else {
-        res.status(404).send('Коллекция не найдена или в ней нет продуктов');
-      }
-    } catch (error) {
-      res.status(500).send('Ошибка сервера');
+  try {
+    const collectionName = req.params.collectionName;
+    const products = await db.collection(collectionName).find().toArray();
+    console.log(products)
+    if (products.length > 0) {
+      res.json(products);
+    } else {
+      res.status(404).send('Коллекция не найдена или в ней нет продуктов');
     }
-  });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).send('User not found');
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).send('Invalid password');
+  }
+
+  const token = jwt.sign({ id: user._id }, 'secret_key', { expiresIn: '1h' });
+  res.json({ token });
+});
+
+// Запуск сервера
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server is Successfully Running, and App is listening on port " + port);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
